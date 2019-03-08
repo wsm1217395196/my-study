3f0d7af577a21fbc054192759e4388d3170a6d5e
wsm <1217395196@qq.com>
2019-03-08 18:00:13 +0800

    HystrixDashboard的使用

--- java/user/src/main/java/com/study/UserStart.java
+++ java/user/src/main/java/com/study/UserStart.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
@@ -12,7 +13,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignClients
 //@EnableHystrix
-//@EnableHystrixDashboard
+@EnableHystrixDashboard
+//@EnableCircuitBreaker
 public class UserStart {
 
     public static void main(String[] args) {
---
+++ java/user/src/main/java/com/study/config/HystrixDashboard.java
@@ -0,0 +1,18 @@
+package com.study.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HystrixDashboard {
+
+//    @Bean
+//    public ServletRegistrationBean getServlet() {
+//        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+//        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+//        registrationBean.setLoadOnStartup(1);
+//        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+//        registrationBean.setName("HystrixMetricsStreamServlet");
+//        return registrationBean;
+//    }
+
+}
