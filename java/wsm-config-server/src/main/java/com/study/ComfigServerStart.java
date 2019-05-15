fb0a733396752c88a50c3d973baba1cfce857e83
wsm <1217395196@qq.com>
2019-05-15 17:57:27 +0800

    无

--- java/wsm-config-server/src/main/java/com/study/ComfigServerStart.java
+++ java/wsm-config-server/src/main/java/com/study/ComfigServerStart.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
-@EnableEurekaClient
 @EnableConfigServer
+@EnableEurekaClient
+@SpringBootApplication
 public class ComfigServerStart {
 
     public static void main(String[] args) {
--- java/wsm-public/src/main/java/com/study/PublicStart.java
+++ java/wsm-public/src/main/java/com/study/PublicStart.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
 @EnableDiscoveryClient
+@SpringBootApplication
 @EnableConfigurationProperties
 public class PublicStart {
 
