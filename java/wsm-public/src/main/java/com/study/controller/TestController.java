e52cc4afe31f8066da111fd6c6db88a542873a77
wsm <1217395196@qq.com>
2019-06-12 11:43:45 +0800

    无

--- java/wsm-public/src/main/java/com/study/controller/TestController.java
+++ java/wsm-public/src/main/java/com/study/controller/TestController.java
@@ -3,6 +3,7 @@ package com.study.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(description = "测试相关控制器")
 @RestController
 @RequestMapping("/test")
-public class TestConfig {
+public class TestController {
 
     @Value("${wsm.name}")
     private String wsmName;
@@ -21,7 +22,7 @@ public class TestConfig {
     private Integer wsmAge;
 
     @ApiOperation("测试读取git配置中的值")
-    @RequestMapping("/getGitConfigValue")
+    @GetMapping("/getGitConfigValue")
     public String getGitConfigValue() {
         System.out.println(wsmName);
         System.out.println(wsmAge);
