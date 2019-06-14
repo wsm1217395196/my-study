511454dd201986c27d3392bb6d89fb671ab8f58d
wsm <1217395196@qq.com>
2019-06-14 10:12:57 +0800

    无

--- java/wsm-public/src/main/java/com/study/controller/TestController.java
+++ java/wsm-public/src/main/java/com/study/controller/TestController.java
@@ -3,6 +3,7 @@ package com.study.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * 测试相关控制器
  */
 @Api(description = "测试相关控制器")
+@RefreshScope
 @RestController
 @RequestMapping("/test")
 public class TestController {
